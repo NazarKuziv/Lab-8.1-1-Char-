2fc25f1d918bbf47293157f0d56976160784cfe6
Nazar Kuziv <nazar123kuziv@gmail.com>
2021-03-12 14:53:56 +0200

    UnitTest

---
+++ UnitTest8.1-1.3/UnitTest8.1-1.3.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Project 8.1-1(char).cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest8113
+{
+	TEST_CLASS(UnitTest8113)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			char s[10] = "jjkjnjSQV";
+
+			int t = Include(s);
+
+			Assert::AreEqual(t, 1);
+		}
+	};
+}
